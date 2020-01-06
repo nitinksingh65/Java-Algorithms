b5318add3811a38587b5c40e446587bc8f4f4fb2
nitinksingh65 <59572483+nitinksingh65@users.noreply.github.com>
2020-01-06 21:41:13 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package p1;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+       int[] arr={1,2,3,4,5,6,7,8};
+
+    }
+}
